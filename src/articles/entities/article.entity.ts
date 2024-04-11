2bffa1c8c91ba59c4d94f8c7b7dbeb9612a2c5c1
rodrigobispo <rodrigoaugustocb@gmail.com>
2024-04-11 16:34:50 -0300

    Devolvendo o autor junto com o artigo; proteção do dado senha, não exibindo.

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Article } from '@prisma/client';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 export class ArticleEntity implements Article {
   @ApiProperty()
@@ -23,6 +24,17 @@ export class ArticleEntity implements Article {
   @ApiProperty()
   updatedAt: Date;
 
-  @ApiProperty({ required: true, nullable: true })
+  @ApiProperty({ required: false, nullable: true })
   authorId: number | null;
+
+  @ApiProperty({ required: true, nullable: true })
+  author?: UserEntity;
+
+  constructor({ author, ...data }: Partial<ArticleEntity>) {
+    Object.assign(this, data);
+
+    if (author) {
+      this.author = new UserEntity(author);
+    }
+  }
 }
